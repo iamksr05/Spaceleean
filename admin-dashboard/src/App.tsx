27165fd03696b1022d5b8f2d77ecd66dd05a032b
Karan Ram <Karanram730005@gmail.com>
2025-12-26 15:14:34 +0530

    security update

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -27,7 +27,7 @@ interface Contact {
 }
 
 const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:3333";
-const ADMIN_PASSWORD = import.meta.env.VITE_ADMIN_PASSWORD;
+const ADMIN_PASSWORD = import.meta.env.ADMIN_PASS;
 console.log("Loaded Env Password:", ADMIN_PASSWORD); // Debugging line
 
 function App() {
